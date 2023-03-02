7a87002406abc2c0b9e489c4d61b0975adaa06da
Defyland <allanflaviosilva@gmail.com>
2023-03-02 18:58:58 -0300

    chore: create a test to api and reponse format

--- src/components/PokemonsList/PokemonsList.spec.tsx
+++ src/components/PokemonsList/PokemonsList.spec.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import {fireEvent, render} from '@testing-library/react-native';
 import {PokemonsList} from '~/components/PokemonsList';
+import {GetPokemonDetails} from '~/data/useCases/GetPokemonDetails';
+import {GetPokemons} from '~/data/useCases/GetPokemons';
+import {AxiosHttpClient} from '~/utils/api';
+import {pokemonDetailsMock} from '~/@mocks/pokemonDetails';
+import {simplePokemonMock} from '~/@mocks/simplePokemon';
+import {IGetPokemons} from '~/data/dataInterfaces';
+
 const MockFN = jest.fn();
 const MockOneFN = jest.fn();
 
@@ -46,6 +53,9 @@ jest.mock('react-redux', () => {
   };
 });
 
+jest.mock('../../data/useCases/GetPokemonDetails');
+jest.mock('../../data/useCases/GetPokemons');
+
 describe('Render PokemonsList', () => {
   it('Should be PokemonsList exist', () => {
     const {getByTestId} = render(<PokemonsList />);
@@ -79,5 +89,67 @@ describe('Render PokemonsList', () => {
       payload: 'https://pokeapi.co/api/v2/pokemon/4/',
       type: 'PokemonDetail/pokemonDetailsFetch',
     });
+  });
+
+  it('call on all Pokemon', async () => {
+    const GetPokemonsMock = GetPokemons as jest.Mocked<typeof GetPokemons>;
+
+    const GetPokemonsInstance = new GetPokemonsMock(AxiosHttpClient);
+
+    const GetPokemonsServiceMock =
+      GetPokemonsInstance.send as jest.MockedFunction<
+        typeof GetPokemonsInstance.send
+      >;
+
+    const response: IGetPokemons.Response = {
+      message: '',
+      status: 'Ok',
+      data: {
+        count: 0,
+        next: '',
+        previus: '',
+        results: [
+          {
+            name: simplePokemonMock.name,
+            url: simplePokemonMock.details,
+          },
+        ],
+      },
+    };
+
+    GetPokemonsServiceMock.mockResolvedValue({
+      message: '',
+      data: response.data,
+      status: 'Ok',
+    });
+    const result = await GetPokemonsInstance.send({});
+    expect(result.status).toBe('Ok');
+    expect(result.data.results).toBe(response.data.results);
+  });
+
+  it('call on details Pokemon Api', async () => {
+    const GetPokemonDetailsMock = GetPokemonDetails as jest.Mocked<
+      typeof GetPokemonDetails
+    >;
+
+    const GetPokemonDetailsInstance = new GetPokemonDetailsMock(
+      AxiosHttpClient
+    );
+
+    const GetPokemonsServiceDetailsMock =
+      GetPokemonDetailsInstance.send as jest.MockedFunction<
+        typeof GetPokemonDetailsInstance.send
+      >;
+
+    GetPokemonsServiceDetailsMock.mockResolvedValue({
+      message: '',
+      data: pokemonDetailsMock,
+      status: 'Ok',
+    });
+    const result = await GetPokemonDetailsInstance.send({
+      pokemonUrl: 'https://pokeapi.co/api/v2/pokemon/bulbasaur',
+    });
+    expect(result.status).toBe('Ok');
+    expect(result.data).toBe(pokemonDetailsMock);
   });
 });
